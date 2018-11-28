dbb5d939bb34c3cfb1dedadaabef0862a9570b49
Samuel Cornet <samuel@noukod.com>
2018-11-28 20:13:25 +0000

    Done.

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -1,10 +1,7 @@
 class CreateStudents < ActiveRecord::Migration[5.1]
   def change
-  create_table :students do |t|
-    t.string :name
-    t.string :genre
-    t.integer :age
-    t.string :hometown
+    create_table :students do |t|
+      t.string :name
+    end
   end
-end
 end
