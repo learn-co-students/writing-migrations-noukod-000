4b79d6ab5d36b7fdce21e8d20876044ec7641683
Merline Belizaire <merline@noukod.com>
2018-11-30 19:39:30 +0000

    Done.

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -1,2 +1,17 @@
 class CreateStudents < ActiveRecord::Migration[5.1]
+  # # def up
+  # #
+  # # end
+  #
+  # def change
+  #   createStudents :students do |t|
+  #       t.string :name
+  #       # t.string :genre
+  #       # t.integer :age
+  #       # t.string :hometown
+  # end
+  #
+  # # def down
+  # #
+  # # end
 end
